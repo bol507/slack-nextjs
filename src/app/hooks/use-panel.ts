f1e6c039da474aef2f3f911a5eadb5aec0cded92
Bolivar Delgado <bolivar.delgado@gmail.com>
2025-03-06 11:40:14 -0500

    feat: use profile member id

--- src/app/hooks/use-panel.ts
+++ src/app/hooks/use-panel.ts
@@ -1,13 +1,24 @@
 import { useParentMessageId } from "@/features/messages/store/use-parent-message-id";
+import { useProfileMemberId } from "@/features/members/store/use-profile-member-id";
+import { set } from "date-fns";
+
 
 export const usePanel = () => {
   const [parentMessageId, setParentMessageId] = useParentMessageId();
+  const [profileMemberId, setProfileMemberId] = useProfileMemberId();
 
   const onOpenMessage = (messageId: string) => {
     setParentMessageId(messageId);
+    setProfileMemberId(null);
   };
 
   const onClose = () => {
+    setParentMessageId(null);
+    setProfileMemberId(null);
+  };
+
+  const onOpenProfile = (memberId: string) => {
+    setProfileMemberId(memberId);
     setParentMessageId(null);
   };
 
@@ -15,5 +26,7 @@ export const usePanel = () => {
     parentMessageId,
     onOpenMessage,
     onClose,
+    profileMemberId,
+    onOpenProfile,
   };
 };
